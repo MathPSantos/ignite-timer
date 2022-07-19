a96cfc059610a3304098d98b55f4123db6fc0e85
Matheus Santos <matheus.psantos2016@gmail.com>
2022-07-19 08:25:22 -0300

    feat: finish History page styles

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,32 @@
+import * as H from "./styles";
+
+export function History() {
+  return (
+    <H.Container>
+      <h1>Meu histórico</h1>
+
+      <H.List>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[1, 2, 3, 4, 5].map((item) => (
+              <tr key={item}>
+                <td>Tarefa 01</td>
+                <td>20 minutos</td>
+                <td>Há cerca de 2 meses</td>
+                <td>Concluído</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </H.List>
+    </H.Container>
+  );
+}
